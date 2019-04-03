2b43101f4b971c0c4bb91280a9946d8608da3a37
santhanakrishnan <santhanakrishnanstark@gmail.com>
2019-04-03 11:36:19 +0530

    Second Commit

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import Project from './Project/Project';
-import  './Project.css';
+import  './Projects.css';
 
 const projects = () => {
     return ( 
         <div className="container mt-5">
             <div className="row">
                 <div className="col-md-8 mx-auto">
-                    <Project 
-                        title="Student Result Analysis System"
-                        description="This is web based tool that gets the semester marks as a 
-                                    input and produce various analysis report for the students."  
-                        language="J2EE, Bootstrap, MySql"/>
-
-                    <Project 
-                        title="Oracle UI"
-                        description="This is web based UI tool to work with oracle database 
-                                    express edition, the user dont need any sql knowledge to work
-                                    with this software"  
-                        language="J2EE, Spring MVC, Bootstrap, Oracle"/>
+                   <div className="row">
+                        <div className="col-md-6">
+                            <Project 
+                            title="Student Result Analysis System"
+                            description="This is web based tool that gets the semester marks as a 
+                                        input and produce various analysis report for the students."  
+                            language="J2EE, Bootstrap, MySql"/>
+                        </div>
+                        <div className="col-md-6">
+                            <Project 
+                            title="Oracle UI"
+                            description="This is web based UI tool to work with oracle database 
+                                        express edition, the user dont need any sql knowledge to work
+                                        with this software"  
+                            language="J2EE, Spring MVC, Bootstrap, Oracle"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
